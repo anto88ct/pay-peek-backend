783416208d26a3795b30a8f466ab9e063c99abd6
Antonio D'Arrigo <antonio.darrigoct@gmail.com>
2025-12-08 16:18:44 +0100

    first commit

---
+++ src/test/java/com/paypeek/backend/PayPeekBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paypeek.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayPeekBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
